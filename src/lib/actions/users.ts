ab40e7538ca50118b4e224f45a8f54c3aac1fd42
Lawrence Eagles <codewitheagles247@gmail.com>
2026-05-04 13:46:26 +0100

    upgraded usersync server function

--- src/lib/actions/users.ts
+++ src/lib/actions/users.ts
@@ -9,16 +9,22 @@ export async function syncUser() {
     const user = await currentUser();
     if (!user) return;
 
-    const existingUser = await prisma.user.findUnique({
-      where: { clerkId: user.id },
-    });
+    const email = user.emailAddresses[0]?.emailAddress;
+    if (!email) return;
 
-    const dbUser = await prisma.user.create({
-      data: {
+    const dbUser = await prisma.user.upsert({
+      where: { clerkId: user.id },
+      update: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email,
+        phone: user.phoneNumbers[0]?.phoneNumber,
+      },
+      create: {
         clerkId: user.id,
         firstName: user.firstName,
         lastName: user.lastName,
-        email: user.emailAddresses[0].emailAddress,
+        email,
         phone: user.phoneNumbers[0]?.phoneNumber,
       },
     });
