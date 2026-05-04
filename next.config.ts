9b0c1b1dcff33543a1e44adae46fad2d3841fd3f
Lawrence Eagles <codewitheagles247@gmail.com>
2026-05-04 11:14:04 +0100

    implemented the home page and setup prisma schema

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
   reactCompiler: true,
 };
 
